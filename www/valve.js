a438e3da7141b76e2276da838470cc95966542d0
Aaron Greco (MBqwertyaaa) <mbqwertyaaa@gmail.com>
2016-05-05 10:23:44 -0400

    Added files via upload

--- www/valve.js
+++ www/valve.js
@@ -1,36 +1,83 @@
 //Check value of page
 function onLoad() {
+	if (localStorage.getItem("valve") == "4"){
+		to4();
+		}
+	if (localStorage.getItem("valve") == "3"){
+		to3();
+		}
+	if (localStorage.getItem("valve") == "2"){
+		to2();
+		}
 	if (localStorage.getItem("valve") == "1"){
-		toFRC();
+		to1();
 		}
 	if (localStorage.getItem("valve") == "0"){
-		toFTC();
+		to0();
+		}
+	if (localStorage.getItem("valve") === null){
+		to0();
 		}
 		}
-//Set page to FTC
-function toFTC() {
-    var team = document.getElementsByClassName("team");
-	team[1].style.display="none";
-	team[0].style.display="initial";
-    team[0].style.textIndent="initial";
-	var e=document.getElementById("left");
-		e.className="off";
-	var e=document.getElementById("right");
-		e.className="on";
-		localStorage.setItem("valve", "0");
-	document.title = "C5CC - FTC " + page;
-}
 
-//Set page to FRC
-function toFRC() {
-	var team = document.getElementsByClassName("team");
-    team[0].style.display="none";
-    team[1].style.display="initial";
-    team[1].style.textIndent="initial";
-	var e=document.getElementById("right");
-		e.className="off";
-	var e=document.getElementById("left");
-		e.className="on";
-		localStorage.setItem("valve", "1");
-	document.title = "C5CC - FRC " + page;
+function to0() {
+    var graph = document.getElementsByClassName("Body");
+	graph[0].style.display="initial";
+	graph[1].style.display="none";
+	graph[2].style.display="none";
+	graph[3].style.display="none";
+	graph[4].style.display="none";
+	localStorage.setItem("valve", "0");
+}
+function to1() {
+    var graph = document.getElementsByClassName("Body");
+	graph[0].style.display="none";
+	graph[1].style.display="initial";
+	graph[2].style.display="none";
+	graph[3].style.display="none";
+	graph[4].style.display="none";
+	localStorage.setItem("valve", "1");
+}
+function to2() {
+    var graph = document.getElementsByClassName("Body");
+	graph[0].style.display="none";
+	graph[1].style.display="none";
+	graph[2].style.display="initial";
+	graph[3].style.display="none";
+	graph[4].style.display="none";
+	localStorage.setItem("valve", "2");
+}
+function to3() {
+    var graph = document.getElementsByClassName("Body");
+	graph[0].style.display="none";
+	graph[1].style.display="none";
+	graph[2].style.display="none";
+	graph[3].style.display="initial";
+	graph[4].style.display="none";
+	localStorage.setItem("valve", "3");
+}
+function to4() {
+    var graph = document.getElementsByClassName("Body");
+	graph[0].style.display="none";
+	graph[1].style.display="none";
+	graph[2].style.display="none";
+	graph[3].style.display="none";
+	graph[4].style.display="initial";
+	localStorage.setItem("valve", "4");
+}
+function prevG() {
+	var valve = localStorage.getItem("valve");
+	if(valve >= 1){
+		valve--;
+		localStorage.setItem("valve", valve);
+		onLoad();
+	}
+}
+function nextG() {
+	var valve = localStorage.getItem("valve");
+	if(valve <= 3){
+		valve++;
+		localStorage.setItem("valve", valve);
+		onLoad();
+	}
 }
